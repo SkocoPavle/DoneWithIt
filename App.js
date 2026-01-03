089f231efeae8f4091ad166fc645cc7e71a83b92
Skoco Pavle <skocopavle@gmail.com>
2026-01-03 18:19:36 +0100

    flex directions

--- App.js
+++ App.js
@@ -8,20 +8,24 @@ export default function App() {
     <View style={{
       backgroundColor: "#fff",
       flex: 1,
+      flexDirection: 'row',
     }}>
       <View style = {{
         backgroundColor: "dodgerblue",
-        flex: 2,
+        width: 100,
+        height: 100,
       }}/>
       
       <View style = {{
         backgroundColor: "yellow",
-        flex: 1,
+        width: 100,
+        height: 100,
       }}/>
 
       <View style = {{
         backgroundColor: "tomato",
-        flex: 1,
+        width: 100,
+        height: 100,
       }}/>
     </View>
   );
